c54e7c5f6577801a21413b51b0a257a6c9bb4e3a
dvdgatik <dvd_gatik@hotmail.com>
2025-01-15 00:03:39 -0600

    Scope of variables

---
+++ scope-of-variables.js
@@ -0,0 +1,11 @@
+/* Scope of variables is fundamental in Javascript and 
+understanding how and where you can use a variable in a program. */
+
+// global scope
+let gretting = 'Hello World!'
+
+ function greet() {
+    console.log(gretting)
+ }
+
+ greet()
